6d4cea683266bbaaa3d93e8234ebc1b25b13f9e3
chromiumboy <chromium.boy@gmail.com>
2025-04-05 13:31:56 -0500

    Certain navmap blips are offset by the pipe layer they are on

--- Content.Shared/Atmos/Consoles/Components/AtmosMonitoringConsoleComponent.cs
+++ Content.Shared/Atmos/Consoles/Components/AtmosMonitoringConsoleComponent.cs
@@ -116,9 +116,15 @@ public struct AtmosDeviceNavMapData
     public Color PipeColor;
 
     /// <summary>
+    /// The pipe layer the entity is on
+    /// </summary>
+    public byte PipeLayer;
+
+    /// <summary>
     /// Populate the atmos monitoring console nav map with a single entity
     /// </summary>
-    public AtmosDeviceNavMapData(NetEntity netEntity, NetCoordinates netCoordinates, int netId, ProtoId<NavMapBlipPrototype> navMapBlip, Direction direction, Color pipeColor)
+    public AtmosDeviceNavMapData
+        (NetEntity netEntity, NetCoordinates netCoordinates, int netId, ProtoId<NavMapBlipPrototype> navMapBlip, Direction direction, Color pipeColor, byte pipeLayer)
     {
         NetEntity = netEntity;
         NetCoordinates = netCoordinates;
@@ -126,6 +132,7 @@ public struct AtmosDeviceNavMapData
         NavMapBlip = navMapBlip;
         Direction = direction;
         PipeColor = pipeColor;
+        PipeLayer = pipeLayer;
     }
 }
 
